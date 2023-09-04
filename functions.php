de6ac1dde14c01d3a22f06a2a8184ded7273c0a8
JanLukasGithub <jan@garloff.de>
2023-09-04 22:50:48 +0200

    Move script inclusion into the module's file
    To prevent the script being loaded for all, even unrelated, pages

--- functions.php
+++ functions.php
@@ -140,10 +140,6 @@ function my_transit_lines_scripts() {
 
 	wp_enqueue_script( 'my-transit-lines-navigation', get_template_directory_uri() . '/js/navigation.js', array(), wp_get_theme()->version, true );
 	wp_enqueue_script( 'my-transit-lines-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), wp_get_theme()->version, true );
-	
-	// enqueue script for the MTL all proposals tile list
-	wp_enqueue_script( 'my-transit-lines-tilelist', get_template_directory_uri() . '/modules/mtl-tile-list/mtl-tile-list.js', array(), wp_get_theme()->version);
-
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );

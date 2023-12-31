55e9dc98ad375595815549c7465a02d5cd20f43e
JanLukasGithub <jan@garloff.de>
2023-12-31 13:31:41 +0100

    Move editing-related code to mtl-proposal-form

--- js/my-transit-lines.js
+++ js/my-transit-lines.js
@@ -6,12 +6,8 @@ const MIN_ZOOM = 0;
 const MAX_ZOOM = 19;
 const MAX_ZOOM_OEPNV_MAP = 18;
 const MAX_ZOOM_OPENTOPO_MAP = 17;
-const SELECTED_Z_INDEX = 1;
 const UNSELECTED_Z_INDEX = 0;
 const ICON_SIZE_UNSELECTED = 21;
-const ICON_SIZE_SELECTED = 23;
-const COLOR_SELECTED = '#07f';
-const STROKE_WIDTH_SELECTED = 3;
 const STROKE_WIDTH_UNSELECTED = 4;
 const TEXT_X_OFFSET = 15;
 const ZOOM_ANIMATION_DURATION = 100;
@@ -92,91 +88,6 @@ var showLabels = true;
 var lowOpacity = true;
 var mapColor = true;
 var fullscreen = false;
-var selectedFeatureIndex = -1;
-var snapping = true;
-var warningMessage = '';
-
-class InteractionControl extends ol.control.Control {
-	constructor(opt_options) {
-		const options = opt_options || {};
-
-		const element = document.createElement('div');
-		element.className = 'interaction-control ol-control';
-
-		super({
-			element: element,
-			target: options.target,
-		});
-
-		this.pointButton = this.createButton('Point', themeUrl + '/images/drawPoint.png');
-		this.lineStringButton = this.createButton('LineString', themeUrl + '/images/drawLineString.png');
-		this.polygonButton = this.createButton('Polygon', themeUrl + '/images/drawPolygon.png');
-		this.circleButton = this.createButton('Circle', themeUrl + '/images/drawCircle.png');
-		this.modifyButton = this.createButton('Modify', themeUrl + '/images/modifyFeature.png');
-		this.selectButton = this.createButton('Select', themeUrl + '/images/selectFeatureAddName.png');
-		this.deleteButton = this.createButton('Delete', themeUrl + '/images/deleteFeatures.png');
-		this.deleteButton.classList.add('unselectable');
-		this.navigateButton = this.createButton('Navigate', themeUrl + '/images/navigation.png');
-		this.snappingButton = this.createButton('RemoveSnapping', themeUrl + '/images/removeSnapping.png');
-
-		element.appendChild(this.pointButton);
-		element.appendChild(this.lineStringButton);
-		element.appendChild(this.polygonButton);
-		element.appendChild(this.circleButton);
-		element.appendChild(this.modifyButton);
-		element.appendChild(this.selectButton);
-		element.appendChild(this.deleteButton);
-		element.appendChild(this.navigateButton);
-		element.appendChild(this.snappingButton);
-	}
-
-	createButton(value, path) {
-		const button = document.createElement('button');
-		button.type = 'button';
-		button.className = 'interaction-control';
-		button.style.backgroundImage = 'url(' + path + ')';
-		button.value = value;
-		button.title = objectL10n[value];
-
-		button.addEventListener('click', this.handleClick.bind(this), false);
-
-		return button;
-	}
-
-	handleClick(event) {
-		var target = event.target;
-
-		if (target == this.deleteButton) {
-			deleteSelected();
-			return;
-		}
-
-		if (target == this.snappingButton) {
-			toggleSnapping();
-
-			if (snapping) {
-				this.snappingButton.style.backgroundImage = 'url(' + themeUrl + '/images/removeSnapping.png)';
-				this.snappingButton.title = objectL10n['RemoveSnapping'];
-			} else {
-				this.snappingButton.style.backgroundImage = 'url(' + themeUrl + '/images/addSnapping.png)';
-				this.snappingButton.title = objectL10n['AddSnapping'];
-			}
-
-			return;
-		}
-
-		for (const node of target.parentElement.childNodes) {
-			node.classList.remove('selected');
-		}
-
-		$('.mtl-tool-hint').css('display','none');
-		$('.mtl-tool-hint.' + target.value).css('display','inline');
-
-		target.classList.add('selected');
-
-		setInteraction(target.value);
-	}
-}
 
 class OptionsControl extends ol.control.Control {
 	constructor(opt_options) {
@@ -319,64 +230,20 @@ const view = new ol.View({
 	maxZoom: MAX_ZOOM,
 });
 
-const interactionControl = new InteractionControl();
 const optionsControl = new OptionsControl();
 const attributionControl = new ol.control.Attribution({
 	collapsible: true,
 	collapsed: false,
 });
 
-// global so we can remove them later
-let drawInteraction;
-const modifyInteraction = new ol.interaction.Modify({ source: vectorSource });
-const dragBoxInteraction = new ol.interaction.DragBox();
-const selectInteraction = new ol.interaction.Select({ layers: [vectorLayer], multi: true, style: styleFunction });
-const snapInteraction = new ol.interaction.Snap({ source: vectorSource });
-
-const selectedFeatures = selectInteraction.getFeatures();
-
 const map = new ol.Map({
-	controls: [new ol.control.Zoom(), new ol.control.ScaleLine(), attributionControl, optionsControl].concat(editMode ? [interactionControl] : []),
+	controls: [new ol.control.Zoom(), new ol.control.ScaleLine(), attributionControl, optionsControl],
 	layers: [attributionLayer, backgroundTileLayer, overlayTileLayer, vectorLayer],
 	target: MAP_ID,
 	view: view,
 });
 
-vectorLayer.on('sourceready', handleSourceReady);
-
-dragBoxInteraction.on('boxend', handleBoxSelect);
-dragBoxInteraction.on('boxstart', function (event) {
-	if (!ol.events.condition.shiftKeyOnly(event.mapBrowserEvent))
-		selectedFeatures.clear();
-});
-
-modifyInteraction.on('modifystart', function (event) {
-	for (var feature of event.features.getArray()) {
-		handleFeatureModified(feature);
-	}
-});
-modifyInteraction.on('modifyend', function (event) {
-	for (var feature of event.features.getArray()) {
-		feature.unset('size');
-	}
-});
-
-selectedFeatures.on('add', handleFeatureSelected);
-selectedFeatures.on('remove', handleFeatureUnselected);
-
-//Notify the user when about to leave page without saving changes
-$(window).bind('beforeunload', function() {
-	if (warningMessage != '') return warningMessage;
-});
-
-if (editMode) {
-	$('#title, #description').on('input propertychange paste', function() {
-		warningMessage = objectL10n.confirmLeaveWebsite;
-	});
-	$('input.cat-select').change(function() {
-		warningMessage = objectL10n.confirmLeaveWebsite;
-	});
-}
+vectorLayer.on('sourceready', importAllWKT);
 
 $(document).ready(function(){
 	// Proposal contact form
@@ -390,24 +257,24 @@ $(document).ready(function(){
 
 // returns the style for the given feature
 function styleFunction(feature) {
-	const colorUnselected = transportModeStyleData[getCategoryOf(feature)][0];
-	const unselected = selectedFeatures.getArray().indexOf(feature) < 0;
+	if (editMode && selectedFeatures.getArray().indexOf(feature) > -1)
+		return selectedStyleFunction(feature);
+
+	const color = transportModeStyleData[getCategoryOf(feature)][0];
 
 	const fillStyle = new ol.style.Fill({
-		color: unselected ? (colorUnselected + '40') : (COLOR_SELECTED + '4'),
+		color: color + '40',
 	});
 
-	const iconSize = unselected ? ICON_SIZE_UNSELECTED : ICON_SIZE_SELECTED;
-
 	const imageStyle = new ol.style.Icon({
-		src: transportModeStyleData[getCategoryOf(feature)][unselected ? 1 : 2],
-		width: iconSize,
-		height: iconSize,
+		src: transportModeStyleData[getCategoryOf(feature)][1],
+		width: ICON_SIZE_UNSELECTED,
+		height: ICON_SIZE_UNSELECTED,
 	});
 
 	const strokeStyle = new ol.style.Stroke({
-		color: unselected ? colorUnselected : COLOR_SELECTED,
-		width: unselected ? STROKE_WIDTH_UNSELECTED : STROKE_WIDTH_SELECTED,
+		color: color,
+		width: STROKE_WIDTH_UNSELECTED,
 	});
 
 	var text = ((showLabels ? feature.get('name') : '') || '') + (feature.get('size') ? '\n' + feature.get('size') : '');
@@ -424,7 +291,7 @@ function styleFunction(feature) {
 		overflow: true,
 	});
 
-	const zIndex = unselected ? UNSELECTED_Z_INDEX : SELECTED_Z_INDEX;
+	const zIndex = UNSELECTED_Z_INDEX;
 
 	return new ol.style.Style({
 		fill: fillStyle,
@@ -433,28 +300,6 @@ function styleFunction(feature) {
 		text: textStyle,
 		zIndex: zIndex,
 	});
-}
-
-// returns the style for the given feature while being drawn
-function drawStyleFunction(feature) {
-	style = ol.style.Style.createEditingStyle()[feature.getGeometry().getType()];
-
-	style[0].text_ = new ol.style.Text({
-		font: 'bold 11px sans-serif',
-		text: feature.get('size') || '',
-		textAlign: 'left',
-		fill: new ol.style.Fill({
-			color: 'white',
-		}),
-		stroke: new ol.style.Stroke({
-			color: COLOR_SELECTED,
-			width: STROKE_WIDTH_SELECTED,
-		}),
-		offsetX: TEXT_X_OFFSET,
-		overflow: true,
-	});
-
-	return style;
 }
 
 /**
@@ -521,149 +366,6 @@ function getSelectedCategory() {
 // redraws the map to update color/icons
 function redraw() {
 	vectorSource.dispatchEvent('change');
-}
-
-function unselectAllFeatures() {
-	selectedFeatures.clear();
-}
-
-// removes all selected features
-function deleteSelected() {
-	var featureArray = Array();
-	for (var i = selectedFeatures.getArray().length; i > 0; i--) {
-		featureArray.push(selectedFeatures.getArray()[i - 1]);
-		selectedFeatures.removeAt(i - 1);
-	}
-	featureArray.forEach(function (feature) {
-		vectorSource.removeFeature(feature);
-	});
-}
-
-// Open textinput for feature name and show size of feature
-function handleFeatureSelected(event) {
-	interactionControl.deleteButton.classList.remove('unselectable');
-
-	$('#feature-textinput').val(event.element.get('name'));
-	$('.feature-textinput-box').slideDown();
-	$('.set-name').css('display', 'block');
-	$('.set-name').click(function () {
-		unselectAllFeatures();
-	});
-	selectedFeatureIndex = vectorSource.getFeatures().indexOf(event.element);
-
-	event.element.set('size', getFeatureSize(event.element));
-}
-
-// Set name of unselected feature to name from the textinput and remove size being shown
-function handleFeatureUnselected(event) {
-	if (selectedFeatures.getArray().length == 0)
-		interactionControl.deleteButton.classList.add('unselectable');
-
-	if (vectorSource.getFeatures().indexOf(event.element) == selectedFeatureIndex) {
-		vectorSource.getFeatures()[selectedFeatureIndex].set('name', $('#feature-textinput').val());
-		selectedFeatureIndex = -1;
-		$('#feature-textinput').val('');
-		$('.feature-textinput-box').slideUp();
-		$('.set-name').css('display', 'none');
-	}
-
-	event.element.unset('size');
-}
-
-// Show the size of modified features
-function handleFeatureModified(feature) {
-	feature.set('size', getFeatureSize(feature));
-	feature.on('change', function () {
-		if (feature.get('size'))
-			feature.set('size', getFeatureSize(feature));
-	});
-}
-
-// Selects all features inside the box dragged for selection
-function handleBoxSelect() {
-	const boxExtent = dragBoxInteraction.getGeometry().getExtent();
-
-	// if the extent crosses the antimeridian process each world separately
-	const worldExtent = map.getView().getProjection().getExtent();
-	const worldWidth = ol.extent.getWidth(worldExtent);
-	const startWorld = Math.floor((boxExtent[0] - worldExtent[0]) / worldWidth);
-	const endWorld = Math.floor((boxExtent[2] - worldExtent[0]) / worldWidth);
-
-	for (let world = startWorld; world <= endWorld; ++world) {
-		const left = Math.max(boxExtent[0] - world * worldWidth, worldExtent[0]);
-		const right = Math.min(boxExtent[2] - world * worldWidth, worldExtent[2]);
-		const extent = [left, boxExtent[1], right, boxExtent[3]];
-
-		const boxFeatures = vectorSource
-			.getFeaturesInExtent(extent)
-			.filter(
-				(feature) =>
-					!selectedFeatures.getArray().includes(feature) &&
-					feature.getGeometry().intersectsExtent(extent)
-			);
-
-		// features that intersect the box geometry are added to the
-		// collection of selected features
-		selectedFeatures.extend(boxFeatures);
-	}
-}
-
-// Runs after the sourceready event was fired
-function handleSourceReady() {
-	importAllWKT();
-
-	addSaveEventListeners();
-}
-
-/**
- * Sets only the specified interaction removing all others
- * 
- * @param {string} interactionType 
- */
-function setInteraction(interactionType) {
-	removeInteractions();
-
-	switch (interactionType) {
-		case 'Circle':
-			alert(objectL10n.circleNotSupported);
-		case 'LineString':
-		case 'Point':
-		case 'Polygon':
-			drawInteraction = new ol.interaction.Draw({ source: vectorSource, type: interactionType, style: drawStyleFunction });
-			drawInteraction.on('drawstart', function (event) {
-				handleFeatureModified(event.feature);
-			});
-			drawInteraction.on('drawend', function (event) {
-				event.feature.unset('size');
-			});
-			map.addInteraction(drawInteraction);
-			map.addInteraction(snapInteraction);
-			break;
-		case 'Select':
-			map.addInteraction(dragBoxInteraction);
-			map.addInteraction(selectInteraction);
-			break;
-		case 'Modify':
-			map.addInteraction(modifyInteraction);
-			map.addInteraction(snapInteraction);
-			break;
-		case 'Navigate':
-			break;
-		default:
-			throw 'Unrecognised interaction type';
-	}
-}
-
-/**
- * Removes all interactions
- */
-function removeInteractions() {
-	map.removeInteraction(drawInteraction);
-	selectedFeatures.clear();
-	map.removeInteraction(selectInteraction);
-	map.removeInteraction(modifyInteraction);
-	map.removeInteraction(snapInteraction);
-	map.removeInteraction(dragBoxInteraction);
 }
 
 // Removes all features from the layer
@@ -734,7 +436,7 @@ function importToMapWKT(source, labelsSource, categorySource, vector = vectorSou
 		labelIndex++;
 	}
 
-	if (selectedFeatureIndex) {
+	if (editMode && selectedFeatureIndex) {
 		selectedFeatureIndex += features.length;
 	}
 
@@ -777,7 +479,7 @@ function importToMapJSON(source, categorySource) {
 
 	features = addCircles(features);
 
-	if (selectedFeatureIndex) {
+	if (editMode && selectedFeatureIndex) {
 		selectedFeatureIndex += features.length;
 	}
 
@@ -960,64 +662,4 @@ function addCircles(features) {
 	}
 
 	return result;
-}
-
-/**
- * Returns the amount of stations in the features array
- * 
- * @param {FeatureLike[]} features the array of features to "search" for stations
- * @returns the amount of stations placed on the map
- */
-function getCountStations(features = vectorSource.getFeatures()) {
-	var count = 0;
-	for (var feature of features) {
-		if (feature.getGeometry() instanceof ol.geom.Point)
-			count++;
-	}
-	return count;
-}
-
-/**
- * Returns the combined length of the lines in the features array
- * 
- * @param {FeatureLike[]} features the array of features to "search" for lines
- * @returns the combined length of the lines placed on the map
- */
-function getLineLength(features = vectorSource.getFeatures()) {
-	var length = 0.0;
-	for (var feature of features) {
-		if (feature.getGeometry() instanceof ol.geom.LineString) {
-			length += ol.sphere.getLength(feature.getGeometry());
-		}
-	}
-	return length;
-}
-
-/**
- * Saves the WKT data of the features array passed into the function to the HTML <input> elements.
- * The data is saved to the DB when the user saves the proposal
- * 
- * @param {FeatureLike[]} features the array of features to save
- */
-function saveToHTML(features = vectorSource.getFeatures()) {
-	warningMessage = objectL10n.confirmLeaveWebsite;
-
-	var wkt_strings = exportToWKT(features);
-
-	// write WKT features data to html element (will be saved to database on form submit)
-	$('#mtl-feature-data').val(wkt_strings[0]);
-	$('#mtl-feature-labels-data').val(wkt_strings[1]);
-	$('#mtl-count-stations').val(getCountStations(features));
-	$('#mtl-line-length').val(getLineLength(features));
-}
-
-/**
- * Adds event listeners to save to HTML automatically
- */
-function addSaveEventListeners() {
-	if (editMode) {
-		vectorSource.on('addfeature', () => {saveToHTML()});
-		vectorSource.on('changefeature', () => {saveToHTML()});
-		vectorSource.on('removefeature', () => {saveToHTML()});
-	}
 }
---
+++ modules/mtl-proposal-form/mtl-proposal-form.js
@@ -0,0 +1,382 @@
+/**
+ * (C) by Jan Garloff and Johannes Bouchain - stadtkreation.de
+ */
+
+const SELECTED_Z_INDEX = 1;
+const ICON_SIZE_SELECTED = 23;
+const COLOR_SELECTED = '#07f';
+const STROKE_WIDTH_SELECTED = 3;
+
+var selectedFeatureIndex = -1;
+var snapping = true;
+var warningMessage = '';
+
+class InteractionControl extends ol.control.Control {
+	constructor(opt_options) {
+		const options = opt_options || {};
+
+		const element = document.createElement('div');
+		element.className = 'interaction-control ol-control';
+
+		super({
+			element: element,
+			target: options.target,
+		});
+
+		this.pointButton = this.createButton('Point', themeUrl + '/images/drawPoint.png');
+		this.lineStringButton = this.createButton('LineString', themeUrl + '/images/drawLineString.png');
+		this.polygonButton = this.createButton('Polygon', themeUrl + '/images/drawPolygon.png');
+		this.circleButton = this.createButton('Circle', themeUrl + '/images/drawCircle.png');
+		this.modifyButton = this.createButton('Modify', themeUrl + '/images/modifyFeature.png');
+		this.selectButton = this.createButton('Select', themeUrl + '/images/selectFeatureAddName.png');
+		this.deleteButton = this.createButton('Delete', themeUrl + '/images/deleteFeatures.png');
+		this.deleteButton.classList.add('unselectable');
+		this.navigateButton = this.createButton('Navigate', themeUrl + '/images/navigation.png');
+
+		element.appendChild(this.pointButton);
+		element.appendChild(this.lineStringButton);
+		element.appendChild(this.polygonButton);
+		element.appendChild(this.circleButton);
+		element.appendChild(this.modifyButton);
+		element.appendChild(this.selectButton);
+		element.appendChild(this.deleteButton);
+		element.appendChild(this.navigateButton);
+	}
+
+	createButton(value, path) {
+		const button = document.createElement('button');
+		button.type = 'button';
+		button.className = 'interaction-control';
+		button.style.backgroundImage = 'url(' + path + ')';
+		button.value = value;
+		button.title = objectL10n[value];
+
+		button.addEventListener('click', this.handleClick.bind(this), false);
+
+		return button;
+	}
+
+	handleClick(event) {
+		var target = event.target;
+
+		if (target != this.deleteButton) {
+			for (const node of target.parentElement.childNodes) {
+				node.classList.remove('selected');
+			}
+
+			$('.mtl-tool-hint').css('display','none');
+			$('.mtl-tool-hint.' + target.value).css('display','inline');
+
+			target.classList.add('selected');
+		} else {
+			deleteSelected();
+			return;
+		}
+
+		setInteraction(target.value);
+	}
+}
+
+// global so we can remove them later
+let drawInteraction;
+const modifyInteraction = new ol.interaction.Modify({ source: vectorSource });
+const dragBoxInteraction = new ol.interaction.DragBox();
+const selectInteraction = new ol.interaction.Select({ layers: [vectorLayer], multi: true, style: styleFunction });
+const snapInteraction = new ol.interaction.Snap({ source: vectorSource });
+
+const selectedFeatures = selectInteraction.getFeatures();
+
+const interactionControl = new InteractionControl();
+map.addControl(interactionControl);
+
+vectorLayer.on('sourceready', addSaveEventListeners);
+
+dragBoxInteraction.on('boxend', handleBoxSelect);
+dragBoxInteraction.on('boxstart', function (event) {
+	if (!ol.events.condition.shiftKeyOnly(event.mapBrowserEvent))
+		selectedFeatures.clear();
+});
+
+modifyInteraction.on('modifystart', function (event) {
+	for (var feature of event.features.getArray()) {
+		handleFeatureModified(feature);
+	}
+});
+modifyInteraction.on('modifyend', function (event) {
+	for (var feature of event.features.getArray()) {
+		feature.unset('size');
+	}
+});
+
+selectedFeatures.on('add', handleFeatureSelected);
+selectedFeatures.on('remove', handleFeatureUnselected);
+
+//Notify the user when about to leave page without saving changes
+$(window).bind('beforeunload', function() {
+	if (warningMessage != '') return warningMessage;
+});
+
+$('#title, #description').on('input propertychange paste', function() {
+    warningMessage = objectL10n.confirmLeaveWebsite;
+});
+$('input.cat-select').change(function() {
+    warningMessage = objectL10n.confirmLeaveWebsite;
+});
+
+// returns the style for a selected feature
+function selectedStyleFunction(feature) {
+    const fillStyle = new ol.style.Fill({
+		color: COLOR_SELECTED + '4',
+	});
+
+	const imageStyle = new ol.style.Icon({
+		src: transportModeStyleData[getCategoryOf(feature)][2],
+		width: ICON_SIZE_SELECTED,
+		height: ICON_SIZE_SELECTED,
+	});
+
+	const strokeStyle = new ol.style.Stroke({
+		color: COLOR_SELECTED,
+		width: STROKE_WIDTH_SELECTED,
+	});
+
+	var text = ((showLabels ? feature.get('name') : '') || '') + (feature.get('size') ? '\n' + feature.get('size') : '');
+
+	const textStyle = new ol.style.Text({
+		font: 'bold 11px sans-serif',
+		text: text,
+		textAlign: 'left',
+		fill: new ol.style.Fill({
+			color: 'white',
+		}),
+		stroke: strokeStyle,
+		offsetX: TEXT_X_OFFSET,
+		overflow: true,
+	});
+
+	const zIndex = SELECTED_Z_INDEX;
+
+	return new ol.style.Style({
+		fill: fillStyle,
+		image: imageStyle,
+		stroke: strokeStyle,
+		text: textStyle,
+		zIndex: zIndex,
+	});
+}
+
+// returns the style for the given feature while being drawn
+function drawStyleFunction(feature) {
+	style = ol.style.Style.createEditingStyle()[feature.getGeometry().getType()];
+
+	style[0].text_ = new ol.style.Text({
+		font: 'bold 11px sans-serif',
+		text: feature.get('size') || '',
+		textAlign: 'left',
+		fill: new ol.style.Fill({
+			color: 'white',
+		}),
+		stroke: new ol.style.Stroke({
+			color: COLOR_SELECTED,
+			width: STROKE_WIDTH_SELECTED,
+		}),
+		offsetX: TEXT_X_OFFSET,
+		overflow: true,
+	});
+
+	return style;
+}
+
+function unselectAllFeatures() {
+	selectedFeatures.clear();
+}
+
+// removes all selected features
+function deleteSelected() {
+	var featureArray = Array();
+	for (var i = selectedFeatures.getArray().length; i > 0; i--) {
+		featureArray.push(selectedFeatures.getArray()[i - 1]);
+		selectedFeatures.removeAt(i - 1);
+	}
+	featureArray.forEach(function (feature) {
+		vectorSource.removeFeature(feature);
+	});
+}
+
+// Open textinput for feature name and show size of feature
+function handleFeatureSelected(event) {
+	interactionControl.deleteButton.classList.remove('unselectable');
+
+	$('#feature-textinput').val(event.element.get('name'));
+	$('.feature-textinput-box').slideDown();
+	$('.set-name').css('display', 'block');
+	$('.set-name').click(function () {
+		unselectAllFeatures();
+	});
+	selectedFeatureIndex = vectorSource.getFeatures().indexOf(event.element);
+
+	event.element.set('size', getFeatureSize(event.element));
+}
+
+// Set name of unselected feature to name from the textinput and remove size being shown
+function handleFeatureUnselected(event) {
+	if (selectedFeatures.getArray().length == 0)
+		interactionControl.deleteButton.classList.add('unselectable');
+
+	if (vectorSource.getFeatures().indexOf(event.element) == selectedFeatureIndex) {
+		vectorSource.getFeatures()[selectedFeatureIndex].set('name', $('#feature-textinput').val());
+		selectedFeatureIndex = -1;
+		$('#feature-textinput').val('');
+		$('.feature-textinput-box').slideUp();
+		$('.set-name').css('display', 'none');
+	}
+
+	event.element.unset('size');
+}
+
+// Show the size of modified features
+function handleFeatureModified(feature) {
+	feature.set('size', getFeatureSize(feature));
+	feature.on('change', function () {
+		if (feature.get('size'))
+			feature.set('size', getFeatureSize(feature));
+	});
+}
+
+// Selects all features inside the box dragged for selection
+function handleBoxSelect() {
+	const boxExtent = dragBoxInteraction.getGeometry().getExtent();
+
+	// if the extent crosses the antimeridian process each world separately
+	const worldExtent = map.getView().getProjection().getExtent();
+	const worldWidth = ol.extent.getWidth(worldExtent);
+	const startWorld = Math.floor((boxExtent[0] - worldExtent[0]) / worldWidth);
+	const endWorld = Math.floor((boxExtent[2] - worldExtent[0]) / worldWidth);
+
+	for (let world = startWorld; world <= endWorld; ++world) {
+		const left = Math.max(boxExtent[0] - world * worldWidth, worldExtent[0]);
+		const right = Math.min(boxExtent[2] - world * worldWidth, worldExtent[2]);
+		const extent = [left, boxExtent[1], right, boxExtent[3]];
+
+		const boxFeatures = vectorSource
+			.getFeaturesInExtent(extent)
+			.filter(
+				(feature) =>
+					!selectedFeatures.getArray().includes(feature) &&
+					feature.getGeometry().intersectsExtent(extent)
+			);
+
+		// features that intersect the box geometry are added to the
+		// collection of selected features
+		selectedFeatures.extend(boxFeatures);
+	}
+}
+
+/**
+ * Sets only the specified interaction removing all others
+ * 
+ * @param {string} interactionType 
+ */
+function setInteraction(interactionType) {
+	removeInteractions();
+
+	switch (interactionType) {
+		case 'Circle':
+			alert(objectL10n.circleNotSupported);
+		case 'LineString':
+		case 'Point':
+		case 'Polygon':
+			drawInteraction = new ol.interaction.Draw({ source: vectorSource, type: interactionType, style: drawStyleFunction });
+			drawInteraction.on('drawstart', function (event) {
+				handleFeatureModified(event.feature);
+			});
+			drawInteraction.on('drawend', function (event) {
+				event.feature.unset('size');
+			});
+			map.addInteraction(drawInteraction);
+			map.addInteraction(snapInteraction);
+			break;
+		case 'Select':
+			map.addInteraction(dragBoxInteraction);
+			map.addInteraction(selectInteraction);
+			break;
+		case 'Modify':
+			map.addInteraction(modifyInteraction);
+			map.addInteraction(snapInteraction);
+			break;
+		case 'Navigate':
+			break;
+		default:
+			throw 'Unrecognised interaction type';
+	}
+}
+
+/**
+ * Removes all interactions
+ */
+function removeInteractions() {
+	map.removeInteraction(drawInteraction);
+	selectedFeatures.clear();
+	map.removeInteraction(selectInteraction);
+	map.removeInteraction(modifyInteraction);
+	map.removeInteraction(snapInteraction);
+	map.removeInteraction(dragBoxInteraction);
+}
+
+/**
+ * Returns the amount of stations in the features array
+ * 
+ * @param {FeatureLike[]} features the array of features to "search" for stations
+ * @returns the amount of stations placed on the map
+ */
+function getCountStations(features = vectorSource.getFeatures()) {
+	var count = 0;
+	for (var feature of features) {
+		if (feature.getGeometry() instanceof ol.geom.Point)
+			count++;
+	}
+	return count;
+}
+
+/**
+ * Returns the combined length of the lines in the features array
+ * 
+ * @param {FeatureLike[]} features the array of features to "search" for lines
+ * @returns the combined length of the lines placed on the map
+ */
+function getLineLength(features = vectorSource.getFeatures()) {
+	var length = 0.0;
+	for (var feature of features) {
+		if (feature.getGeometry() instanceof ol.geom.LineString) {
+			length += ol.sphere.getLength(feature.getGeometry());
+		}
+	}
+	return length;
+}
+
+/**
+ * Saves the WKT data of the features array passed into the function to the HTML <input> elements.
+ * The data is saved to the DB when the user saves the proposal
+ * 
+ * @param {FeatureLike[]} features the array of features to save
+ */
+function saveToHTML(features = vectorSource.getFeatures()) {
+	warningMessage = objectL10n.confirmLeaveWebsite;
+
+	var wkt_strings = exportToWKT(features);
+
+	// write WKT features data to html element (will be saved to database on form submit)
+	$('#mtl-feature-data').val(wkt_strings[0]);
+	$('#mtl-feature-labels-data').val(wkt_strings[1]);
+	$('#mtl-count-stations').val(getCountStations(features));
+	$('#mtl-line-length').val(getLineLength(features));
+}
+
+/**
+ * Adds event listeners to save to HTML automatically
+ */
+function addSaveEventListeners() {
+    vectorSource.on('addfeature', () => {saveToHTML()});
+    vectorSource.on('changefeature', () => {saveToHTML()});
+    vectorSource.on('removefeature', () => {saveToHTML()});
+}

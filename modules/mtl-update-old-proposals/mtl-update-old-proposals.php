82a54c798dd8422f5333c48a3498f76d24ed042b
JanLukasGithub <jan@garloff.de>
2024-05-10 01:56:26 +0200

    Restructure update-old-proposals module
    Also add mtl-remove-wkt shortcode

--- modules/mtl-update-old-proposals/mtl-update-old-proposals.php
+++ modules/mtl-update-old-proposals/mtl-update-old-proposals.php
@@ -10,9 +10,9 @@
 /* created by Jan Garloff, 2024-04-22 */
 
 /**
- * shortcode [mtl-update-old-proposals]
+ * shortcode [mtl-tag-adding]
  */
-function mtl_update_old_proposals_output($atts) {
+function mtl_tag_adding_output() {
 
 	$mtl_options3 = get_option('mtl-option-name3');
 
@@ -25,7 +25,7 @@ function mtl_update_old_proposals_output($atts) {
 	$tags = get_terms('post_tag', array('fields' => 'ids'));
 	$args = array(
 		'post_type' => 'mtlproposal',
-		'posts_per_page' => -1,
+		'posts_per_page' => 1,
 		'tax_query' => array(
 			array(
 				'taxonomy' => 'post_tag',
@@ -38,7 +38,7 @@ function mtl_update_old_proposals_output($atts) {
 	$the_query = new WP_Query($args);
 
 	if (!$the_query->have_posts())
-		return mtl_update_old_proposals_serverside();
+		return '<p>All proposals have tags on them</p>';
 
 	$the_query->the_post();
 	global $post;
@@ -46,13 +46,11 @@ function mtl_update_old_proposals_output($atts) {
 	$output = '<script id="mtl-data-script" type="text/javascript"> var data = "' . str_replace(array("\r", "\n"), "", get_post_meta($post->ID, 'mtl-feature-data', true)) . '";</script>';
 
 	if (!isset($_POST['no-reload']) || !$_POST['no-reload']) {
-		$output .= mtl_update_old_proposals_serverside();
-
 		$output .= '<script id="mtl-source-script" type="text/javascript"> var countrySource = \'' . str_replace(array("\r", "\n"), "", file_get_contents($mtl_options3['mtl-country-source'])) . '\';' . "\r\n";
 		$output .= 'var stateSource = \'' . str_replace(array("\r", "\n"), "", file_get_contents($mtl_options3['mtl-state-source'])) . '\';' . "\r\n";
 		$output .= 'var districtSource = \'' . str_replace(array("\r", "\n"), "", file_get_contents($mtl_options3['mtl-district-source'])) . '\'; </script>' . "\r\n";
 	
-		wp_enqueue_script('mtl-tag-adding', get_template_directory_uri().'/modules/mtl-update-old-proposals/mtl-update-old-proposals.js', array('openlayers'), wp_get_theme()->version, true);
+		wp_enqueue_script('mtl-tag-adding', get_template_directory_uri().'/modules/mtl-update-old-proposals/mtl-tag-adding.js', array('openlayers'), wp_get_theme()->version, true);
 	}
 
 	$output .= '<strong><a id="post-title" href="'.get_permalink($post->ID).'">'.$post->post_title.'</a></strong>';
@@ -64,14 +62,14 @@ function mtl_update_old_proposals_output($atts) {
 
 	$output .= '<form id="add_tags" name="add_tags" method="post" action="" enctype="multipart/form-data"><input type="hidden" id="id_input" name="mtl-id" value="'.$post->ID.'" /><input type="hidden" name="no-reload" id="no-reload" value="" /><input type="hidden" name="tags" id="tags_input" /><input type="submit" id="form-submit" value="Add tags" /></form>';
 
+	wp_reset_postdata();
+
 	return $output;
 }
-add_shortcode( 'mtl-update-old-proposals', 'mtl_update_old_proposals_output' );
+add_shortcode( 'mtl-tag-adding', 'mtl_tag_adding_output' );
 
 /**
- * Updates all old proposals that only need serverside updating
- * 
- * @return string html containing how many proposals were updated
+ * shortcode [mtl-remove-meta]
  */
 function mtl_update_old_proposals_serverside() {
 	$count = 0;
@@ -82,6 +80,7 @@ function mtl_update_old_proposals_serverside() {
 
 	return '<p>'.$count.' proposal(s) were updated server-side</p>';
 }
+add_shortcode( 'mtl-remove-meta', 'mtl_update_old_proposals_serverside' );
 
 function mtl_under_construction_deletion_callback($post, $key) {
 	wp_update_post( array(
@@ -132,3 +131,56 @@ function mtl_modify_meta_key($post_type, $max_num, $key, $callback = null) {
 
 	return $count;
 }
+
+/** 
+ * shortcode [mtl-remove-wkt]
+ */
+function mtl_remove_WKT_output() {
+	if (current_user_can('administrator') && isset($_POST['mtl-id']) && isset($_POST['mtl-features']) && get_post_type($_POST['mtl-id']) == 'mtlproposal' && !get_post_meta($_POST['mtl-id'], 'mtl-features')) {
+
+		update_post_meta($_POST['mtl-id'], 'mtl-features', $_POST['mtl-features']);
+
+		delete_post_meta($_POST['mtl-id'], 'mtl-feature-data');
+		delete_post_meta($_POST['mtl-id'], 'mtl-feature-labels-data');
+
+	}
+
+	$args = array(
+		'post_type' => 'mtlproposal',
+		'posts_per_page' => 1,
+		'meta_query' => array(
+			'relation' => 'OR',
+			array(
+				'key' => 'mtl-feature-data',
+				'compare' => 'EXISTS',
+			),
+			array(
+				'key' => 'mtl-feature-labels-data',
+				'compare' => 'EXISTS',
+			),
+		),
+	);
+	$the_query = new WP_Query($args);
+
+	if (!$the_query->have_posts()) {
+		return '<p>No proposals are using WKT</p>';
+	}
+	
+	$the_query->the_post();
+	global $post;
+
+	$output = '<script type="text/javascript" id="mtl-data-script"> var vectorData = "'.str_replace(array("\n", "\r"), "", get_post_meta($post->ID,'mtl-feature-data',true)).'"; var vectorLabelsData = "'.str_replace(array("\n", "\r"), "", get_post_meta($post->ID,'mtl-feature-labels-data',true)).'"; </script>'."\r\n";
+		
+	if (!isset($_POST['no-reload']) || !$_POST['no-reload']) {
+		wp_enqueue_script('mtl-remove-wkt', get_template_directory_uri().'/modules/mtl-update-old-proposals/mtl-remove-wkt.js', array('openlayers'), wp_get_theme()->version, true);
+	}
+	
+	$output .= '<strong><a id="post-title" href="'.get_permalink($post->ID).'">'.$post->post_title.'</a></strong>';
+
+	$output .= '<form id="remove_wkt" name="remove_wkt" method="post" action="" enctype="multipart/form-data"><input type="hidden" id="id_input" name="mtl-id" value="'.$post->ID.'" /><input type="hidden" name="no-reload" id="no-reload" value="" /><input type="hidden" name="mtl-features" id="features_input" /><input type="submit" id="form-submit" value="Remove WKT" /></form>';
+
+	wp_reset_postdata();
+
+	return $output;
+}
+add_shortcode( 'mtl-remove-wkt', 'mtl_remove_WKT_output' );
